b778bfd8493ee706d199f118012e864b65d785a3
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-06-19 10:37:10 +0530

    Time: 22 ms (14.54%) | Memory: 46.1 MB (82.46%) - LeetSync

--- 169-majority-element/majority-element.cs
+++ 169-majority-element/majority-element.cs
@@ -1,31 +1,39 @@
 public class Solution {
     public int MajorityElement(int[] nums) {
-
-        Dictionary<int , int > freq = new Dictionary<int,int>();
         int n = nums.Length;
 
-        foreach(int num in nums){
-            if(freq.ContainsKey(num)){
-                freq[num]++;
-
-            }else{
-                freq[num]=1;
-            }
-        }
-        int maxcount  =0;
-        int maxele=0;
-
-        foreach(var num in freq){
-
-            if(num.Value>maxcount){
-                maxcount = num.Value;
-                maxele=num.Key;
-
-
-            }
-
-        }
-        return maxele;
+        // //brute force O(n) O(n)
+        // Dictionary<int , int > freq = new Dictionary<int,int>();
         
+
+        // foreach(int num in nums){
+        //     if(freq.ContainsKey(num)){
+        //         freq[num]++;
+
+        //     }else{
+        //         freq[num]=1;
+        //     }
+        // }
+        // int maxcount  =0;
+        // int maxele=0;
+
+        // foreach(var num in freq){
+
+        //     if(num.Value>maxcount){
+        //         maxcount = num.Value;
+        //         maxele=num.Key;
+
+
+        //     }
+
+        // }
+        // return maxele;
+
+     //2-->   //using sorting majrtiy ele will always at n/2
+
+        Array.Sort(nums);
+        return nums[n/2];
+
+                
     }
 }
