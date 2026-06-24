83766d7bc79457b009f48883747f7399c632cbf5
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-06-24 16:34:05 +0530

    Time: 4 ms (100.00%) | Memory: 95.6 MB (14.67%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cs
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cs
@@ -2,40 +2,62 @@ public class Solution {
     public int[] RearrangeArray(int[] nums) {
 
 
-       // bruite force using 2 list
+    //    // bruite force using 2 list
 
-       List<int> pos = new List<int>();
-       List<int> neg = new List<int>();
+    //    List<int> pos = new List<int>();
+    //    List<int> neg = new List<int>();
 
-       foreach(int num in nums){
+    //    foreach(int num in nums){
+
+    //     if(num>0){
+    //         pos.Add(num);
+    //     }else{
+    //         neg.Add(num);
+    //     }
+    //    }
+
+    //    //add both
+
+    //    int p =0;
+    //    int n=0;
+
+
+    //    for(int i=0; i<nums.Length; i++){
+
+    //     if(i%2==0){
+    //         nums[i]=pos[p++];
+    //     }else{
+    //         nums[i]=neg[n++];
+    //     }
+
+
+
+    //    }
+
+
+    //   return nums;
+
+    //optimal slighltly better o(n) Tc sc
+
+    int[] ans= new int[nums.Length];
+
+    int pos =0;
+    int neg=1;
+
+
+    foreach(int num in nums){
 
         if(num>0){
-            pos.Add(num);
+            ans[pos]=num;
+            pos+=2;
         }else{
-            neg.Add(num);
-        }
-       }
-
-       //add both
-
-       int p =0;
-       int n=0;
-
-
-       for(int i=0; i<nums.Length; i++){
-
-        if(i%2==0){
-            nums[i]=pos[p++];
-        }else{
-            nums[i]=neg[n++];
+            ans[neg]=num;
+            neg+=2;
         }
 
+    }
 
-
-       }
-
-
-      return nums;
+    return ans;
         
     }
 }
