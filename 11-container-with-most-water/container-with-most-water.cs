d62d14f668c64ccbe844a4401b39f329bc659aca
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-06-27 15:51:15 +0530

    Time: 2 ms (15.59%) | Memory: 62 MB (22.97%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cs
@@ -0,0 +1,48 @@
+public class Solution {
+    public int MaxArea(int[] height) {
+        
+        // //brute force 
+        // int maxwater =0;
+
+        // for(int i=0;i<height.Length;i++){
+
+        //     for(int j=i+1;j<height.Length;j++){
+
+              
+
+        //         int len=j-i;
+
+        //         int b = Math.Min(height[i],height[j]);
+
+        //         maxwater = Math.Max(maxwater,len*b);
+        //     }
+        // }
+
+        // return maxwater;
+
+        //optimal using two ptr
+
+        int maxwater=0;
+        int left=0;
+        int right=height.Length-1;
+
+        while(left<right){
+
+            int len = Math.Min(height[left],height[right]);
+
+            int b = right-left;
+
+            maxwater=Math.Max(maxwater,len*b);
+
+            //move ptr with smaller h
+
+            if(height[left]<height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+
+        return maxwater;
+    }
+}
