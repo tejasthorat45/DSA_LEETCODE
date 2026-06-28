427771c3b4c5a7e5041797183137c4fb6ad9f79e
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-06-28 20:02:55 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (86.95%) - LeetSync

--- 27-remove-element/remove-element.cs
+++ 27-remove-element/remove-element.cs
@@ -1,16 +1,35 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
 
-        int k=0;
-        for(int i=0;i<nums.Length;i++){
+        // int k=0;
+        // for(int i=0;i<nums.Length;i++){
 
-            if(nums[i]!=val){
+        //     if(nums[i]!=val){
 
-                nums[k++]=nums[i];
+        //         nums[k++]=nums[i];
+        //     }
+        // }
+
+        // return k;
+
+        //more optimal using the two pte
+
+        int left=0;
+        int right=nums.Length-1;
+
+        while(left<=right){
+
+            //check the eleemnt 
+            if(nums[left]==val){
+                nums[left]=nums[right];
+                right--;
+            }else{
+                left++;
             }
+
         }
 
-        return k;
+        return right+1;
 
 
 
