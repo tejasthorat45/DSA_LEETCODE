b2557f57a3e653f171ee6e34864241ec563cc8e2
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-07-01 12:36:30 +0530

    Time: 2 ms (15.86%) | Memory: 51.7 MB (34.48%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cs
+++ 941-sort-array-by-parity/sort-array-by-parity.cs
@@ -1,21 +1,51 @@
 public class Solution {
     public int[] SortArrayByParity(int[] nums) {
 
-        int[] ans = new int[nums.Length];
-        int idx=0;
-        int odd = ans.Length-1;
+        // int[] ans = new int[nums.Length];
+        // int idx=0;
+        // int odd = ans.Length-1;
 
 
-        for(int i=0;i<nums.Length;i++){
+        // for(int i=0;i<nums.Length;i++){
 
-            if(nums[i]%2==0){
-                ans[idx++]=nums[i];
-            }else{
-                ans[odd--]=nums[i];
+        //     if(nums[i]%2==0){
+        //         ans[idx++]=nums[i];
+        //     }else{
+        //         ans[odd--]=nums[i];
+        //     }
+        // }
+
+        // return ans;
+
+        //using the twpo ptr
+
+        int left =0;
+        int right=nums.Length-1;
+
+        while(left<right){
+
+            while(left<right && nums[left]%2==0) //stop at odd no
+            left++;
+
+            while(left<right && nums[right]%2!=0) //stop at even no
+            right--;
+
+            //swap them
+            if(left<right){
+                int temp = nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+
+                left++;
+                right--;
             }
+
+
+            
         }
 
-        return ans;
+        return nums;
+
 
 
         
