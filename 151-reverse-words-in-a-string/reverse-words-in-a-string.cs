08f7590649a52c5aad2212e431622a4a5e9bf070
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-07-01 11:49:01 +0530

    Time: 5 ms (38.29%) | Memory: 42.3 MB (43.51%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cs
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cs
@@ -1,11 +1,43 @@
 public class Solution {
     public string ReverseWords(string s) {
 
-        string[] words = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+        // string[] words = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
 
-        Array.Reverse(words);
+        // Array.Reverse(words);
 
-        return string.Join(" ",words);
+        // return string.Join(" ",words);
+
+        StringBuilder sb = new StringBuilder();
+
+        int i=s.Length-1;
+
+        while(i>=0){
+
+            //skip spaces
+
+            while(i>=0 && s[i] ==' ')
+            i--;
+
+            if(i<0)
+            break;
+
+            //mark end of word
+            int j=i;
+            //move i begining of word
+
+            while(i>=0 && s[i]!=' ')
+            i--;
+            //append word
+            if(sb.Length>0)
+            sb.Append(' ');
+
+            sb.Append(s.Substring(i+1,j-i));
+        }
+
+        return sb.ToString();
+
+
+
 
         
     }
