763e66f3092eb5f30a3a6f3cdf67a3869c6308cb
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-07-04 20:09:56 +0530

    Time: 4 ms (69.48%) | Memory: 47.4 MB (45.45%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cs
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cs
@@ -1,24 +1,42 @@
 public class Solution {
     public int[] Intersection(int[] nums1, int[] nums2) {
 
-        List<int> ans = new List<int>();
+        // List<int> ans = new List<int>();
       
 
 
 
-        foreach(int num in nums1){
+        // foreach(int num in nums1){
 
-            foreach(int nam in nums2){
+        //     foreach(int nam in nums2){
 
-                if(num==nam && !ans.Contains(num)){
+        //         if(num==nam && !ans.Contains(num)){
 
-                    ans.Add(num);
+        //             ans.Add(num);
 
-                }
+        //         }
+        //     }
+        // }
+
+        // return ans.ToArray();
+        
+
+
+        //using the HASHSET
+
+        HashSet<int> temp = new HashSet<int>(nums1);
+        HashSet<int> res = new HashSet<int>();
+
+        foreach(int num in nums2 ){
+
+            if(temp.Contains(num)){
+                res.Add(num);
+
             }
         }
 
-        return ans.ToArray();
-        
+        return res.ToArray();
+
+
     }
 }
