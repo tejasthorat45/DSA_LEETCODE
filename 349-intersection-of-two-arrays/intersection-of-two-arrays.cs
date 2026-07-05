610346d41c0f5186ee0f289c58401580c2b3f38e
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-07-05 11:02:50 +0530

    Time: 4 ms (69.73%) | Memory: 47.4 MB (45.67%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cs
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cs
@@ -22,20 +22,57 @@ public class Solution {
         
 
 
-        //using the HASHSET
+        // //using the HASHSET  O(n*m)  O(k) SC
 
-        HashSet<int> temp = new HashSet<int>(nums1);
-        HashSet<int> res = new HashSet<int>();
+        // HashSet<int> temp = new HashSet<int>(nums1);
+        // HashSet<int> res = new HashSet<int>();
 
-        foreach(int num in nums2 ){
+        // foreach(int num in nums2 ){
 
-            if(temp.Contains(num)){
-                res.Add(num);
+        //     if(temp.Contains(num)){
+        //         res.Add(num);
 
+        //     }
+        // }
+
+        // return res.ToArray();
+
+        //using sorting and Two ptr
+
+        Array.Sort(nums1);
+        Array.Sort(nums2);
+
+
+        List<int> res = new List<int>();
+
+        int i=0;
+        int j=0;
+
+
+        while(i<nums1.Length && j<nums2.Length){
+
+
+
+            if(nums1[i]==nums2[j]){
+
+                //ADDD TO RES
+                //CHECK IF THERE IS DUPLICATE
+
+                if(res.Count==0 || res[res.Count-1]!=nums1[i]){
+                    res.Add(nums1[i]);
+                }
+                i++;
+                j++;
+            }else if(nums1[i]>nums2[j]){
+                j++;
+            }else{
+                i++;
             }
         }
 
         return res.ToArray();
+
+
 
 
     }
