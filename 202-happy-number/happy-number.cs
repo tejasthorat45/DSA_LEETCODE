82e9ab2c40cd3f8d51581117cc17e4f2cb3f5689
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-07-15 18:29:56 +0530

    Time: 3 ms (63.27%) | Memory: 31.6 MB (40.97%) - LeetSync

--- 202-happy-number/happy-number.cs
+++ 202-happy-number/happy-number.cs
@@ -1,16 +1,32 @@
 public class Solution {
     public bool IsHappy(int n) {
 
-        for(int i=0;i<1000;i++){
-            if(n==1)
-            return true;
+        // for(int i=0;i<1000;i++){
+        //     if(n==1)
+        //     return true;
+
+        //     n=nextNo(n);
+
+
+        // }
+
+        // return false;
+
+        //using the hashset
+
+        HashSet<int> visited = new HashSet<int>();
+
+        while(n!=1){
+
+            if(visited.Contains(n))
+            return false;
+
+            visited.Add(n);
 
             n=nextNo(n);
-
-
         }
 
-        return false;
+        return true;
 
         
     }
