fe210a25e1abc1cd7fb653df3023f859abcdc740
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-07-16 16:19:33 +0530

    Time: 90 ms (93.41%) | Memory: 48.4 MB (98.84%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cs
+++ 141-linked-list-cycle/linked-list-cycle.cs
@@ -12,13 +12,35 @@
 public class Solution {
     public bool HasCycle(ListNode head) {
 
-        HashSet<ListNode> visited = new HashSet<ListNode>();
+        // HashSet<ListNode> visited = new HashSet<ListNode>();
 
-        while(head!=null){
-            if(visited.Contains(head))
+        // while(head!=null){
+        //     if(visited.Contains(head))
+        //     return true;
+        //     visited.Add(head);
+        //     head=head.next;
+        // }
+
+        // return false;
+
+        //optimal solution 0(1)
+
+        //using the fast and slow ptr
+
+        //edge case'
+        if(head==null || head.next==null)
+        return false;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next!=null){
+
+            slow=slow.next;
+            fast=fast.next.next;
+
+            if(slow==fast)
             return true;
-            visited.Add(head);
-            head=head.next;
         }
 
         return false;
