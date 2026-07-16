f10edab14f87952da5223c19a6034ab0104a305a
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-07-16 16:53:18 +0530

    Time: 103 ms (10.86%) | Memory: 47.5 MB (88.49%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cs
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cs
@@ -13,16 +13,40 @@ public class Solution {
     public ListNode DetectCycle(ListNode head) {
 
         //using space hashset
-        HashSet<ListNode> visited = new HashSet<ListNode>();
+        // HashSet<ListNode> visited = new HashSet<ListNode>();
 
-        while(head != null){
-            if(visited.Contains(head))
-            return head;
-            visited.Add(head);
-            head=head.next;
+        // while(head != null){
+        //     if(visited.Contains(head))
+        //     return head;
+        //     visited.Add(head);
+        //     head=head.next;
+        // }
+
+        // return null;
+
+        //using two ptr
+        ListNode slow=head;
+        ListNode fast =head;
+
+        while(fast!=null && fast.next!=null){
+
+            slow=slow.next;
+            fast=fast.next.next;
+
+            if(slow==fast){
+
+                ListNode ptr=head;
+                while(slow!=ptr){
+                    slow=slow.next;
+                    ptr=ptr.next;
+                }
+                return ptr;
+            }
+
         }
-
         return null;
+
+        
 
         
     }
