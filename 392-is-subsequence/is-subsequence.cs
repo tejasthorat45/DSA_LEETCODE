108f3a8e1aaa4843aba01297a4087c5cb9b2ba47
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-07-17 21:59:22 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (46.05%) - LeetSync

--- 392-is-subsequence/is-subsequence.cs
+++ 392-is-subsequence/is-subsequence.cs
@@ -1,35 +1,57 @@
 public class Solution {
     public bool IsSubsequence(string s, string t) {
 
-        //brute force approach
+        // //brute force approach
 
 
-        int start =0;
-        foreach(char c in s ){
-            bool found = false;
+        // int start =0;
+        // foreach(char c in s ){
+        //     bool found = false;
 
-            while(start<t.Length){
+        //     while(start<t.Length){
 
                 
 
-                if(t[start]==c){
-                    found =true;
-                    start++;
+        //         if(t[start]==c){
+        //             found =true;
+        //             start++;
 
-                    break;
+        //             break;
 
-                }
-                start++;
+        //         }
+        //         start++;
 
-            }
+        //     }
 
-              if(!found)
-               return false;
-        }
+        //       if(!found)
+        //        return false;
+        // }
 
       
 
+        // return true;
+
+        //using two pointer 
+
+        int i=0;
+        int j=0;
+
+        while(i<s.Length && j<t.Length){
+
+            if(s[i]==t[j]){
+                i++;
+                
+            }
+
+            j++;
+        }
+
+        if(i==s.Length)
         return true;
+
+        return false;
+
+
         
     }
 }
