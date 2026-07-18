9422f6a67485d1b8b1dc6665046f0c2cc7caf428
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-07-18 16:14:37 +0530

    Time: 4 ms (71.40%) | Memory: 69.9 MB (61.79%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cs
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cs
@@ -1,19 +1,44 @@
 public class Solution {
     public int FindDuplicate(int[] nums) {
 
-        //
+        // //O(nlogn)
 
-        Array.Sort(nums);
+        // Array.Sort(nums);
 
-        for(int i=1;i<nums.Length;i++){
+        // for(int i=1;i<nums.Length;i++){
 
-            if(nums[i]==nums[i-1]){
-                return nums[i];
-            }
+        //     if(nums[i]==nums[i-1]){
+        //         return nums[i];
+        //     }
+
+        // }
+
+        // return 0;
+
+        //using fast and slow ptr
+
+        int slow =nums[0];
+        int fast=nums[0];
+
+        slow=nums[slow];
+        fast=nums[nums[fast]];
+
+        while(slow != fast){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+
 
         }
 
-        return 0;
+        //detect the cycle 
+        slow=nums[0];
+
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+
+        return slow;
         
     }
 }
