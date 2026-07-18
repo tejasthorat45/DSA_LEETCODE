75a93f8a4b2578edf575b23d0c2eb455976a0b8e
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-07-18 20:11:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (6.76%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cs
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cs
@@ -12,18 +12,34 @@
 public class Solution {
     public ListNode MiddleNode(ListNode head) {
 
-        //store in Lisy 
-        List<ListNode> nodes = new List<ListNode>();
+        // //store in Lisy 
+        // List<ListNode> nodes = new List<ListNode>(); //O(n)
         
 
-        // adde the nodes tothe list
-        while(head != null){
+        // // adde the nodes tothe list
+        // while(head != null){
 
-            nodes.Add(head);
+        //     nodes.Add(head);
+        //     head=head.next;
+        // }
+
+        // return nodes[nodes.Count/2];
+
+        // second approach cal length of the LL
+
+        ListNode current = head;
+        int length=0;
+
+        while(current!=null){
+            length++;
+            current=current.next;
+        }
+
+        for(int i=0;i<length/2;i++){
             head=head.next;
         }
 
-        return nodes[nodes.Count/2];
+        return head;
 
 
         
