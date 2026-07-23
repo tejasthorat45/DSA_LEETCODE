ae1519b60892b5647306754ec40f4f787a9b1712
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-07-23 13:08:59 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (82.23%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cs
+++ 874-backspace-string-compare/backspace-string-compare.cs
@@ -1,31 +1,85 @@
 public class Solution {
 
-    private string GetCorrect(string s){
+    // private string GetCorrect(string s){
 
-        StringBuilder sb = new StringBuilder();
+    //     StringBuilder sb = new StringBuilder();
 
-        foreach(char ch in s){
+    //     foreach(char ch in s){
 
-            if(ch=='#'){
-                if(sb.Length>0){
-                    sb.Length--;
-                }
-            }else{
+    //         if(ch=='#'){
+    //             if(sb.Length>0){
+    //                 sb.Length--;
+    //             }
+    //         }else{
 
-                sb.Append(ch);
+    //             sb.Append(ch);
 
-            }
+    //         }
 
             
-        }
+    //     }
 
-        return sb.ToString();
+    //     return sb.ToString();
 
 
-    }
+    // }
     public bool BackspaceCompare(string s, string t) {
 
-        return GetCorrect(s)==GetCorrect(t);
+        // return GetCorrect(s)==GetCorrect(t);
+
+        int m = s.Length;
+        int n = t.Length;
+
+        int i = m-1;
+        int j=n-1;
+
+        int skip_s=0;
+        int skip_t=0;
+
+        while(i>=0 || j>=0){
+
+            while(i>=0){
+                if(s[i]=='#'){
+
+                    skip_s++;
+                    i--;
+
+                }else if(skip_s>0){
+                    skip_s--;
+                    i--;
+                }else{
+                    break;
+                }
+            }
+
+            //same for the t
+
+            while(j>=0){
+                if(t[j]=='#'){
+                    skip_t++;
+                    j--;
+                }else if(skip_t>0){
+                    skip_t--;
+                    j--;
+                }else{
+                    break;
+                }
+            }
+
+            char first = i<0?'$':s[i];
+            char second = j<0 ?'$':t[j];
+
+            if(first!=second)
+            return false;
+
+            i--;
+            j--;
+
+
+        }
+
+
+        return true;
 
 
         
