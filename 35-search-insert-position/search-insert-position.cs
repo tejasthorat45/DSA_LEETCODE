236c9ef74331e32c3e767d8bf7ad6c21d31cc481
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-07-26 21:21:42 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (75.74%) - LeetSync

--- 35-search-insert-position/search-insert-position.cs
+++ 35-search-insert-position/search-insert-position.cs
@@ -3,15 +3,39 @@ public class Solution {
 
         //brute force
 
-        for(int i=0;i<nums.Length;i++){
+        // for(int i=0;i<nums.Length;i++){
 
 
-            if(target<=nums[i]){
-                return i;
+        //     if(target<=nums[i]){
+        //         return i;
+        //     }
+
+        // }
+        // return nums.Length;
+
+        //binary searchb
+
+        int left=0;
+        int right=nums.Length-1;
+
+        while(left<=right){
+
+            int mid = left+(right-left)/2;
+
+            if(target==nums[mid]){
+                return mid;
             }
 
+            if(nums[mid]<target){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+
+
         }
-        return nums.Length;
+
+        return left;
 
 
 
