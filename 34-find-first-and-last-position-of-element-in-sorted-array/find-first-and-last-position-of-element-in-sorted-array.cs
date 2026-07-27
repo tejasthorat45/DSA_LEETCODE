49ff2215c9224b37106f93a80342cbe2073cc85f
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-07-27 15:38:35 +0530

    Time: 0 ms (100.00%) | Memory: 49.4 MB (9.29%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cs
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cs
@@ -1,21 +1,98 @@
 public class Solution {
     public int[] SearchRange(int[] nums, int target) {
 
-        int first =-1;
-        int last=-1;
+        // int first =-1;
+        // int last=-1;
 
-        for(int i=0;i<nums.Length;i++){
+        // for(int i=0;i<nums.Length;i++){
 
 
-            if(nums[i]==target){
+        //     if(nums[i]==target){
 
-                if(first==-1)
-                first=i;
+        //         if(first==-1)
+        //         first=i;
 
-                last=i;
+        //         last=i;
+        //     }
+        // }  
+
+        // return new int[] {first,last}     ;
+
+        int n= nums.Length;
+
+        int left_most = findfirst(nums,target,n);
+
+        if(left_most==-1){
+            return new int[] {-1,-1};
+        }
+
+        int right_most= findlast(nums,target,n);
+
+        return new int[] {left_most,right_most};
+
+
+
+    }
+
+    private int findfirst(int[] nums,int target,int n){
+
+
+        //use bs 
+
+        int l=0;
+        int r=n-1;
+        int left_most=-1;
+
+        while(l<=r){
+
+            //cal mid
+            int mid= l+(r-l)/2;
+
+
+
+            if(nums[mid]==target){
+
+                left_most=mid;
+                r=mid-1;
+
+            }else if(nums[mid]>target){
+                r=mid-1;
+            }else{
+                l=mid+1;
             }
-        }  
 
-        return new int[] {first,last}     ;
+        }
+
+        return left_most;
+    }
+
+    private int findlast(int[] nums,int target , int n){
+
+        int l=0;
+        int r=n-1;
+
+        int right_most=-1;
+
+        //bs 
+
+        while(l<=r){
+
+            int mid = l+(r-l)/2;
+
+            if(nums[mid]==target){
+                right_most=mid;
+                l=mid+1;
+            }else if(nums[mid]<target){
+
+                l=mid+1;
+
+
+            }else{
+                r=mid-1;
+            }
+        }
+
+        return right_most;
+
     }
 }
