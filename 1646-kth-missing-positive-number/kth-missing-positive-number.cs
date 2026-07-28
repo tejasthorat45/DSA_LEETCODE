c288413263ab0dda00b65171a3488f199ce3c0a2
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-07-28 19:08:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (53.78%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cs
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cs
@@ -1,27 +1,57 @@
 public class Solution {
     public int FindKthPositive(int[] arr, int k) {
 
-        int current =1;
-        int i=0;
+        // int current =1;
+        // int i=0;
 
-        while(k>0){
+        // while(k>0){
 
-            if(i<arr.Length && arr[i]==current){
-                i++;
-            }else{
-                k--;
+        //     if(i<arr.Length && arr[i]==current){
+        //         i++;
 
-                if(k==0){
-                    return current;
-                }
-            }
+        //         //arr = [2,3,4,7,11], k = 5
+        //     }else{
+        //         k--;
 
-            current++;
-        }
+        //         if(k==0){
+        //             return current;
+        //         }
+        //     }
+
+        //     current++;
+        // }
 
        
 
-        return current;
+        // return current;
+
+
+        //using the BS 
+
+        int n= arr.Length;
+
+        int l=0;
+        int r=n-1;
+
+        while(l<=r){
+
+            int mid = l+(r-l)/2;
+
+            int missing_value=arr[mid]-(mid+1);
+
+
+            if(missing_value<k){
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+
+
+
+            
+        }
+
+        return l+k;
         
     }
 }
