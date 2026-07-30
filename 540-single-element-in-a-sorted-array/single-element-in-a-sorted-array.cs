1de10e980fda848f6115d23fb60e02e1cbe3892d
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-07-30 10:10:23 +0530

    Time: 0 ms (100.00%) | Memory: 50.5 MB (27.68%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cs
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cs
@@ -1,19 +1,42 @@
 public class Solution {
     public int SingleNonDuplicate(int[] nums) {
 
-        if(nums.Length==1)
-        return nums[0];
+        // if(nums.Length==1)
+        // return nums[0];
 
 
-        for(int i=1;i<nums.Length;i+=2){
+        // for(int i=1;i<nums.Length;i+=2){
 
 
-            if(nums[i]!=nums[i-1]){
-                return nums[i-1];
+        //     if(nums[i]!=nums[i-1]){
+        //         return nums[i-1];
+        //     }
+        // }
+
+        // return nums[nums.Length-1];
+
+
+        //using the binary search
+
+        int l=0;
+        int r=nums.Length-1;
+
+        while(l<r){
+
+
+            int mid = l+(r-l)/2;
+
+            if(mid%2==1)
+            mid--;
+
+            if(nums[mid]==nums[mid+1]){
+                l=mid+2;
+            }else{
+                r=mid;
             }
         }
 
-        return nums[nums.Length-1];
+        return nums[r];
 
 
 
