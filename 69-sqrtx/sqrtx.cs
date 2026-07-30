ef2605a6e2a5dfed035872d8498380e7850d616e
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-07-30 09:12:33 +0530

    Time: 0 ms (100.00%) | Memory: 29 MB (72.55%) - LeetSync

--- 69-sqrtx/sqrtx.cs
+++ 69-sqrtx/sqrtx.cs
@@ -1,19 +1,46 @@
 public class Solution {
     public int MySqrt(int x) {
 
-        //brtute force 
+        // //brtute force 
 
-        //base case
+        // //base case
 
-        if(x==0)
-        return 0;
+        // if(x==0)
+        // return 0;
 
-        long i=1;
-        while(i*i<=x){
-            i++;
+        // long i=1;
+        // while(i*i<=x){
+        //     i++;
+        // }
+
+        // return (int)(i-1);
+
+        int left=0;
+        int right=x;
+
+        int ans = 0;
+
+        while(left<=right){
+
+            int mid = left+(right-left)/2;
+
+            long sqrt = (long)mid*mid;
+            if(sqrt==x){
+                return mid;
+            }
+            else if(sqrt<x){
+                left=mid+1;
+                ans=mid;
+            }else{
+                right=mid-1;
+            }
         }
 
-        return (int)(i-1);
+        return ans;
+
+
+
+
 
 
         
