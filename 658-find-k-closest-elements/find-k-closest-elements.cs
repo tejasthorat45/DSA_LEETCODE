33744a43fb6577d0fc48ec5d63cac23c151c9f17
Tejas Thorat <154748045+tejasthorat45@users.noreply.github.com>
2026-08-02 19:39:00 +0530

    Time: 1 ms (97.63%) | Memory: 63.7 MB (45.56%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cs
+++ 658-find-k-closest-elements/find-k-closest-elements.cs
@@ -5,37 +5,69 @@ public class Solution {
 
         //using the two ptr
 
-        int left=0;
-        int right=arr.Length-1;
+        // int left=0;
+        // int right=arr.Length-1;
 
-        while(right-left+1>k){
-
-
-            if(Math.Abs(arr[left]-x)<=Math.Abs(arr[right]-x)){
-
-                right--;
+        // while(right-left+1>k){
 
 
+        //     if(Math.Abs(arr[left]-x)<=Math.Abs(arr[right]-x)){
+
+        //         right--;
+
+
+
+        //     }else{
+        //         left++;
+        //     }
+
+
+        // }
+
+        // //cal the the res from window
+
+        // List<int> res = new List<int>();
+
+        // for(int i=left;i<=right;i++){
+
+        //     res.Add(arr[i]);
+
+
+        // }
+
+        // return res;
+
+
+        //udsinng the Binary search
+
+        //Input: arr = [1, 2, 3, 4, 5], k = 4, x = 3
+        //              l,m  r 
+        int l=0;
+        int r=arr.Length-k;
+
+        while(l<r){
+
+
+            int mid = l+(r-l)/2;
+
+            if(x-arr[mid]>arr[mid+k]-x){
+                l=mid+1;
 
             }else{
-                left++;
+                r=mid;
             }
-
-
         }
-
-        //cal the the res from window
 
         List<int> res = new List<int>();
 
-        for(int i=left;i<=right;i++){
+        for(int i=l;i<l+k;i++){
 
             res.Add(arr[i]);
-
-
         }
 
         return res;
+
+
 
 
         
